5993b2a43698fdc00231347522c0c59615fe10dc
Kartik Lamkhade <kartiklamkhade7@gmail.com>
2026-07-14 21:23:47 +0530

    ok

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 # Here are your Instructions
-=======
-# myworshipschedul
->>>>>>> 05ddb2957872ff5a305fec287932dc2ec4f7c544
--- backend/server.py
+++ backend/server.py
@@ -2693,5 +2693,3 @@ logger = logging.getLogger(__name__)
 @app.on_event("shutdown")
 async def shutdown_db_client():
     client.close()
-
-
